3d75b80169e24f2d8d086c3c265bcadadd2ea9f0
Jacob Ochoa <jacob.ochoa@gmail.com>
2025-02-01 21:15:59 -0800

    verified (#6)

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
 
+	"github.com/ciph-r/postage/internal/server"
+)
+
+func main() {
+	if err := server.Run(); err != nil {
+		slog.Error("server failed to run", "reason", err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})))
 }
