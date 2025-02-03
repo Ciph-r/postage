792e3a85a55184f4beeee8e19ee6e67a9d8a0ea4
Jacob Ochoa <jacob.ochoa@gmail.com>
2025-02-03 04:35:25 -0800

    config comment

--- internal/server/config.go
+++ internal/server/config.go
@@ -2,6 +2,8 @@ package server
 
 import "github.com/ciph-r/postage/internal/services/health"
 
+// config is the main config of the entire server. All other configs are imported
+// here and given a corresponding namespace using the envPrefix tag.
 type config struct {
 	Health health.Config `envPrefix:"HEALTH_"`
 }
