9a08ef9878cf36bee7ff28423e0ace51da89e8df
Jordan Hunter <88363678+Ciph-r@users.noreply.github.com>
2025-02-05 07:50:31 -0800

    Socket service config
    * config file for sockets service
    
    * Changed socket service to non-privileged port :8080

--- internal/server/config.go
+++ internal/server/config.go
@@ -1,9 +1,13 @@
 package server
 
-import "github.com/ciph-r/postage/internal/services/health"
+import (
+	"github.com/ciph-r/postage/internal/services/health"
+	"github.com/ciph-r/postage/internal/services/sockets"
+)
 
 // config is the main config of the entire server. All other configs are imported
 // here and given a corresponding namespace using the envPrefix tag.
 type config struct {
-	Health health.Config `envPrefix:"HEALTH_"`
+	Health  health.Config  `envPrefix:"HEALTH_"`
+	Sockets sockets.Config `envPrefix:"SOCKETS_"`
 }
--- internal/services/sockets/service.go
+++ internal/services/sockets/service.go
@@ -7,10 +7,11 @@ import (
 	"github.com/ciph-r/postage/internal/services"
 )
 
-func NewService() services.Service {
+func NewService(cfg Config) services.Service {
+
 	srv := &http.Server{
-		Addr:    ":80", // TODO: set addr with a configurable var.
-		Handler: nil,   // TODO: set handler with socket router.
+		Addr:    cfg.Addr,
+		Handler: nil, //TODO: set handler with socket router.
 	}
 	return services.NewHTTP(srv, time.Minute)
 }
