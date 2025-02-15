f104d829690b65ea40b61532d3be2b3f2f978be2
Jacob Ochoa <jacob.ochoa@gmail.com>
2025-02-15 03:31:45 -0800

    rm comment for pr

--- internal/services/forwards/mocks_test.go
+++ internal/services/forwards/mocks_test.go
@@ -9,11 +9,6 @@ import (
 
 // ClientLoadBalancerMock satisfies traffic.ClientLoadBalancer interface.
 type ClientLoadBalancerMock struct {
-	// this is a hack to avoid mocking unused methods. it works similar to
-	// method overloading in other languages. its allows the struct to satisfy
-	// the interface without actually implementing the methods, or in this case,
-	// only implementing the methods we need. this pattern is really only
-	// acceptable in tests for ease of mocking.
 	traffic.LoadBalancer
 
 	ForwardFunc func(ctx context.Context, socketID string, r io.Reader) error
